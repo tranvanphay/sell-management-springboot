594400d6029c8758337588b791099f921b7e3f25
Văn Phây Trần <phay.tran@thlsoft.com>
2020-07-12 21:51:39 +0700

    init

---
+++ src/main/java/com/phaytran/sellmanagement/server/SellManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.phaytran.sellmanagement.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellManagementServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phaytran/sellmanagement/server/SellManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phaytran.sellmanagement.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
